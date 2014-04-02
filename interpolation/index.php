1bf8f335ec6d145e9d1b8eef8a09303724be45ef
Hecatesias <hecatesias@gmail.com>
2014-04-02 13:45:59 +0200

    dir interpolation

---
+++ interpolation/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>Interpolation - Phases</title>
+	<link href="css/bootstrap.css" rel="stylesheet">
+    	<script src="js/jquery-1.10.2.js"></script>
+    	<script src="js/bootstrap.js"></script>
+</head>
+
+<body>
+	<div>
+	<form form method="POST" action="inter1.php" class="form-horizontal">
+	      <fieldset>
+			<!-- Form Name -->
+			     	  <legend>Choisissez votre phase de calcul : </legend>
+				  		     	   <!-- Multiple Radios -->
+							   		 <div class="form-group">
+											<label class="col-md-4 control-label" for="radios">Phase</label>
+											       		       		      <div class="col-md-4">
+															      	      <div class="radio">
+																      	       <label for="radios-0">
+																	       	           <input type="radio" name="radios" id="radios-0" value="1" checked="checked">
+																			   	  	       		     		   	      1
+																										           </label>
+																											       </div>
+																											           <div class="radio">
+																												   	    <label for="radios-1">
+																													    	        <input type="radio" name="radios" id="radios-1" value="2">
+																															       		    		  		  2 et 3
+																																					          </label>
+																																						      </div>
+																																						         </div>
+																																							   </div>
+																																							     <!-- Button -->
+																																							     	  	 <div class="form-group">
+																																									        <label class="col-md-4 control-label" for="singlebutton"></label>
+																																										       		       		      <div class="col-md-4">
+																																														      	      <button id="singlebutton" name="singlebutton" class="btn btn-inverse">Suivant</button>
+																																															      	      					    </div>
+																																																					      </div>
+																																																					       </fieldset>
+																																																					       </form>
+																																																					       </div>
+</body>
+</html>
